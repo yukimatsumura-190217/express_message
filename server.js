c2d4e05b12130675c8f94f7a6e710e289f66f592
yukimatsumura-190217 <190217id@yse-c.net>
2020-12-07 12:14:37 +0900

    add +a

--- server.js
+++ server.js
@@ -21,12 +21,33 @@ app.use((req,res,next)=>{
     next();
 });
 
+//静的コンテンツ
+app.use(express.static(__dirname + '/public'));
+
 app.post('/', (req,res)=>{
     let message = req.body.message;
     let result = {
         message: req.body.message,
         datetime: new Date(),
     };
+    res.send(result);
+});
+
+app.post('/purchase', (req, res) => {
+    const tax_rate = 0.1;
+    const price = req.body.price;
+    const amount = req.body.amount;
+
+    const tax = price * amount * tax_rate;
+    const excluding_price = price * amount;
+    const total_price = excluding_price + tax;
+
+    let result = {
+        excluding_price: excluding_price,
+        total_price: total_price,
+        tax: tax,
+    };
+    //レスポンス
     res.send(result);
 });
 
