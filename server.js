c4827e28d5cffdac3dd47253300b3b3fe56ddcad
yukimatsumura-190217 <190217id@yse-c.net>
2020-12-07 11:41:47 +0900

    add public folder

--- server.js
+++ server.js
@@ -4,6 +4,32 @@ const express = require('express');
 //サーバー作成
 const app = express();
 
+//JSON対応
+app.use(express.json());
+//URLエンコードされたデータを解析
+app.use(express.urlencoded({extended:true}));
+
+//クロスドメインの許可　xss
+app.use((req,res,next)=>{
+    console.log(`middleware: all. url: ${req.url}`);
+
+    //CROS設定　全てのドメインに対して許可
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
+});
+
+app.post('/', (req,res)=>{
+    let message = req.body.message;
+    let result = {
+        message: req.body.message,
+        datetime: new Date(),
+    };
+    res.send(result);
+});
+
 //ルーティング
 //webルートにリクエストされたらレスポンス
 app.get('/',(req,res)=>{
